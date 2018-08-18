a2f19aa4934a674ab4e3171eac0221e6b6e2c83e
pch <pchmx@qq.com>
2018-08-18 11:55:12 +0800

    项目初始化

---
+++ beststore/src/main/java/com/bs/beststore/web/filter/Encode.java
@@ -0,0 +1,33 @@
+package com.bs.beststore.web.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * 字符集过滤器，*表示所有资源
+ */
+@WebFilter("*")
+public class Encode implements Filter {
+
+	public void destroy() {
+
+	}
+
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+
+	}
+
+}
---
+++ beststore/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello BestStore!</h2>
+</body>
+</html>
